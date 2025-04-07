8c3a2596f687e31b320ee4d3af47cbdd69313177
wmikuriy <wmikuriy@amazon.com>
2025-04-07 16:51:23 +0900

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test', '<rootDir>/snapshot_test'],
+  roots: ['<rootDir>/snapshot_test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
